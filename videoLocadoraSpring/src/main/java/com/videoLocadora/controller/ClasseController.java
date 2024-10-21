2513c017e8797ad3e61799fcf5cc13f7a2a0fd67
Marco Antônio de Paula Dornel <thebuss1601@gmail.com>
2024-10-21 09:08:15 -0300

    feat: Adição dos Controllers(Ator, Diretor, Classe)

--- videoLocadoraSpring/src/main/java/com/videoLocadora/controller/ClasseController.java
+++ videoLocadoraSpring/src/main/java/com/videoLocadora/controller/ClasseController.java
@@ -2,14 +2,17 @@ package com.videoLocadora.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.videoLocadora.domain.controle_acervo.Classe;
-import com.videoLocadora.repository.ClasseRepository;
+import com.videoLocadora.dto.ClasseDTO;
+import com.videoLocadora.service.ClasseService;
 
 import lombok.AllArgsConstructor;
 
@@ -18,20 +21,36 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ClasseController {
     
-    private final ClasseRepository classeRepository;
+    private final ClasseService  classeService;
 
     // Lista todas as classes
     @GetMapping("/list")
-    public List<Classe> listarClasses() {
-        return classeRepository.findAll();
+    public List<ClasseDTO> listarClasses() {
+        return classeService.listarClasse();
     }
 
-    // Buscar ator
+    // Buscar classe
     @GetMapping("/list/{id}")
-    public ResponseEntity<Classe> buscarClassePorId(@PathVariable Long id){
-        return classeRepository.findById(id)
-            .map(classe -> ResponseEntity.ok().body(classe))
-            .orElse(ResponseEntity.notFound().build());
+    public ClasseDTO buscarClassePorId(@PathVariable Long id){
+        return classeService.buscarClassePorId(id);
     }
     
+    // Adiciona novo classe
+    @PostMapping("/add")
+    public ClasseDTO adicionarClasse(@RequestBody ClasseDTO classe) {
+        return classeService.adicionarClasse(classe);
+    }
+
+    // Atualiza classe
+    @PutMapping("/update/{id}")
+    public ClasseDTO atualizarClasse(@PathVariable Long id, @RequestBody ClasseDTO classe) {
+        return classeService.atualizarClasse(id, classe);
+    }
+
+    // Deleta classe
+    @DeleteMapping("/delete/{id}")
+    public void deletarClasse(@PathVariable Long id) {
+        classeService.deletarAtor(id);
+    }
+
 }
